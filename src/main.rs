68fc187079472a38e0a0fab9512c2570a0a1e10b
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-06-03 11:20:53 -0700

    Progress on clock configuration

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,64 @@ fn setup_peripherals() {
     let dp = pac::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
 
+    // configuration comes from generated HPL_PMC_CONFIG.H file for SAME70 XPLD sample
+    //	_pmc_init_sources();
+    // 	_pmc_init_master_clock();
+    // 	_pmc_init_program_clock();
+    // 	_pmc_init_fs_clock();
+
+
+    // TODO most of this clock configuration code should move into the HAL
+    const CONF_XOSC20M_STARTUP_TIME: u8 = 62;
+
+    // enable XOSC20M external oscillator (3-20 MHz)
+    //	data = hri_pmc_read_CKGR_MOR_reg(PMC) & ~CKGR_MOR_MOSCXTBY;
+    // 	data |= CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCXTST(CONF_XOSC20M_STARTUP_TIME);
+    // turn off MOSCXTBY external osc bypass
+    dp.PMC.ckgr_mor.modify(|_, w| unsafe {
+        w
+            //disable crystal oscillator bypass
+            .moscxtby().clear_bit()
+            // Enable the Main crystal oscillator by setting CKGR_MOR.MOSCXTEN.
+            .moscxten().set_bit()
+            // Configure the CKGR_MOR.MOSCXTST field with the Main crystal oscillator startup time
+            .moscxtst().bits(CONF_XOSC20M_STARTUP_TIME)
+            // provide password for writing to this critical config register
+            .key().passwd()
+    });
+
+    // Wait for PMC_SR.MOSCXTS flag to rise, indicating the
+    // end of a startup period of the Main crystal oscillator.
+    while dp.PMC.pmc_sr.read().moscxts().bit_is_clear() {
+        // spin until crystal oscillator starts
+        // TODO timeout and retry?
+    }
+
+    dp.PMC.ckgr_mor.modify(|_, w|  {
+        w
+            // select crystal oscillator as main oscillator
+            .moscsel().set_bit()
+            // provide password for writing to this critical config register
+            .key().passwd()
+    });
+
+
     //TODO use PMC to setup clocks
-    let mut pmc = dp.PMC;
+    const ID_PIOC: u8 = pac::Interrupt::PIOC as u8;
+
+    // enable peripheral clock for PIOC (PCER0)
+    //this is how you set PMC_PCSR0, using PMC_PCER0 :
+    dp.PMC.pmc_pcer0.write(|w| unsafe {
+        w.bits( (1 << ID_PIOC) as u32 )
+    });
+
+
+    // clock for PORTC:
+    // peripheral id: ID_PIOC         ( 12) /**< \brief Parallel Input/Output Controller (PIOC) */
+    //		if (!hri_pmc_get_PCSR0_reg(PMC, (1 << periph_id))) {
+    // 			hri_pmc_set_PCSR0_reg(PMC, (1 << periph_id));
+    // 		}
+    // GPIO for LED0: C8  (PORTC)
 
 }
 
